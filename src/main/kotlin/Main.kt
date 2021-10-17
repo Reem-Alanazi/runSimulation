4d009f09cbb779f589509e9cd4cbe1fda2bb4bab
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-17 15:33:43 +0300

    Passing a function reference

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,14 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 fun main() {
 
-    val greetingFunction = { playerName: String, numBuildings: Int ->
+    runSimulation("Guyal", ::printConstructionCost)
+    { playerName, numBuildings ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
-
-}
-    //Calling Fun                 // return nothing so I use double colon
-    runSimulation("Reem",::printConstructionCost,greetingFunction)
+    }
     printConstructionCost(6)
-
 }
-
-
 inline fun runSimulation (playerName : String,
                        costPrinter :(Int) -> Unit,
                    greetingFunction : (String,Int) -> String){
@@ -23,7 +18,6 @@ inline fun runSimulation (playerName : String,
     println(greetingFunction(playerName,numBuildings))
 
 }
-
 fun printConstructionCost(numBuildings: Int) {
     val cost = 500
     println("construction cost: ${cost * numBuildings}")
