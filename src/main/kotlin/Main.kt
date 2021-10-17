2199f008767c9df1ab2d350d0fc918a017002686
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-17 14:51:15 +0300

    greetingFunction

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+fun main() {
+
+    val greetingFunction = { playerName: String, numBuildings: Int ->
+        val currentYear = 2018
+        println("Adding $numBuildings houses")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+
+}
+runSimulation("Reem",greetingFunction)
+
+}
+
+fun runSimulation (playerName : String,
+                   greetingFunction : (String,Int) -> String){
+    val numBuildings = Random.nextInt(1..3)
+    println(greetingFunction(playerName,numBuildings))
+
+}
