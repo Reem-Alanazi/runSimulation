a6a58d3a8f22020cef66091ff9f2390aeadfab0a
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-17 15:37:31 +0300

    Adding the configureGreetingFunction function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,24 @@
-import kotlin.random.Random
-import kotlin.random.nextInt
 fun main() {
 
-    runSimulation("Guyal", ::printConstructionCost)
-    { playerName, numBuildings ->
-        val currentYear = 2018
-        println("Adding $numBuildings houses")
-        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
-    }
-    printConstructionCost(6)
-}
-inline fun runSimulation (playerName : String,
-                       costPrinter :(Int) -> Unit,
-                   greetingFunction : (String,Int) -> String){
-    val numBuildings = Random.nextInt(1..3)
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName,numBuildings))
+    runSimulation()
+
 
 }
-fun printConstructionCost(numBuildings: Int) {
-    val cost = 500
-    println("construction cost: ${cost * numBuildings}")
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
 }
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return { playerName: String ->
+        val currentYear = 2018
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+}
+
 
 
